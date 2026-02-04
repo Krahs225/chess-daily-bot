a90faf13ab3049e3054ab696383da6ae34246222
Krahs225 <brettramak@hotmail.com>
2026-02-04 11:00:25 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = 1468320170891022417  # exact dezelfde
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("🤖 Bot logged in as", client.user)
+
+    try:
+        channel = await client.fetch_channel(CHANNEL_ID)
+        await channel.send("✅ TEST: bot can post messages")
+        print("✅ Message sent")
+    except Exception as e:
+        print("❌ Error while sending message:", e)
+
+    await client.close()
+
+client.run(TOKEN)
