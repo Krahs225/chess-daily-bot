98f48e1efe6bea8baa4735422a0f8f848557d73d
Krahs225 <brettramak@hotmail.com>
2026-03-04 11:33:00 +0100

    Create random_puzzle_check.py

---
+++ random_puzzle_check.py
@@ -0,0 +1,71 @@
+import os
+import requests
+import discord
+import chess
+import chess.svg
+import cairosvg
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+CHANNEL_ID = 1466819168748704021
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+async def post_puzzle(channel):
+
+    r = requests.get("https://api.chess.com/pub/puzzle", timeout=10)
+    data = r.json()
+
+    fen = data["fen"]
+    title = data["title"]
+    url = data["url"]
+
+    board = chess.Board(fen)
+
+    orientation = chess.WHITE
+    if board.turn == chess.BLACK:
+        orientation = chess.BLACK
+
+    svg_board = chess.svg.board(board=board, orientation=orientation)
+
+    cairosvg.svg2png(bytestring=svg_board, write_to="puzzle.png")
+
+    file = discord.File("puzzle.png")
+
+    embed = discord.Embed(
+        title="♟️ Random Chess Puzzle",
+        description=title,
+        color=0x00ff00
+    )
+
+    embed.add_field(name="Puzzle link", value=url)
+    embed.set_image(url="attachment://puzzle.png")
+
+    await channel.send(file=file, embed=embed)
+
+
+@client.event
+async def on_ready():
+
+    channel = await client.fetch_channel(CHANNEL_ID)
+
+    messages = [msg async for msg in channel.history(limit=10)]
+
+    for msg in messages:
+
+        if msg.author.bot:
+            continue
+
+        if msg.content.lower() == "!randompuzzle":
+
+            await post_puzzle(channel)
+            break
+
+    await client.close()
+
+
+client.run(DISCORD_TOKEN)
