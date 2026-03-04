7848840b2acff9e1b920068fcc052505dff93166
Krahs225 <brettramak@hotmail.com>
2026-03-04 13:34:04 +0100

    Update random_puzzle_check.py

--- random_puzzle_check.py
+++ random_puzzle_check.py
@@ -3,21 +3,20 @@ import discord
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-DISCORD_CHANNEL_ID = 1468320170891022417
+CHANNEL_ID = 1468320170891022417
 
 intents = discord.Intents.default()
-intents.message_content = True
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    channel = await client.fetch_channel(DISCORD_CHANNEL_ID)
+    channel = client.get_channel(CHANNEL_ID)
 
-    async for message in channel.history(limit=50):
-        if "!randompuzzle" in message.content:
-            await channel.send("checked")
-            break
+    if channel is None:
+        channel = await client.fetch_channel(CHANNEL_ID)
+
+    await channel.send("bot reached channel")
 
     await client.close()
 
