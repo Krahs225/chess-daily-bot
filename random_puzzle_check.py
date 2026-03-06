b0db3519c0181f9e8a3daa52f85f4cb0c400a4a2
Krahs225 <brettramak@hotmail.com>
2026-03-06 11:37:43 +0100

    Update random_puzzle_check.py

--- random_puzzle_check.py
+++ random_puzzle_check.py
@@ -14,18 +14,17 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-def load_last_message():
+def load_last_command():
     if not os.path.exists(STATE_FILE):
-        return None
-
+        return 0
     with open(STATE_FILE, "r") as f:
         data = json.load(f)
-        return data.get("last_message_id")
+        return data.get("last_command_id", 0)
 
 
-def save_last_message(message_id):
+def save_last_command(message_id):
     with open(STATE_FILE, "w") as f:
-        json.dump({"last_message_id": message_id}, f)
+        json.dump({"last_command_id": message_id}, f)
 
 
 @client.event
@@ -35,20 +34,23 @@ async def on_ready():
 
     channel = await client.fetch_channel(CHANNEL_ID)
 
-    last_message_id = load_last_message()
+    last_command_id = load_last_command()
 
     messages = [msg async for msg in channel.history(limit=100)]
 
+    # sort oldest → newest
+    messages.reverse()
+
     for message in messages:
+
+        if message.id <= last_command_id:
+            continue
 
         if "!randompuzzle" in message.content:
 
-            if message.id == last_message_id:
-                break
-
             await channel.send("checked")
 
-            save_last_message(message.id)
+            save_last_command(message.id)
             break
 
     await client.close()
