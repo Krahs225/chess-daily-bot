c566a7471d22df1f2660f390965cdf69fae04306
Krahs225 <brettramak@hotmail.com>
2026-03-06 11:53:18 +0100

    Update random_puzzle_check.py

--- random_puzzle_check.py
+++ random_puzzle_check.py
@@ -1,12 +1,10 @@
 import os
 import discord
 import asyncio
-import json
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 CHANNEL_ID = 1468320170891022417
-STATE_FILE = "random_state.json"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,44 +12,31 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-def load_last_command():
-    if not os.path.exists(STATE_FILE):
-        return 0
-    with open(STATE_FILE, "r") as f:
-        data = json.load(f)
-        return data.get("last_command_id", 0)
-
-
-def save_last_command(message_id):
-    with open(STATE_FILE, "w") as f:
-        json.dump({"last_command_id": message_id}, f)
-
-
 @client.event
 async def on_ready():
+
+    print("Bot connected")
 
     await asyncio.sleep(3)
 
     channel = await client.fetch_channel(CHANNEL_ID)
 
-    last_command_id = load_last_command()
+    print("Channel found, reading messages...")
 
-    messages = [msg async for msg in channel.history(limit=100)]
+    messages = [msg async for msg in channel.history(limit=5)]
 
-    # sort oldest → newest
     messages.reverse()
 
     for message in messages:
 
-        if message.id <= last_command_id:
+        if message.author.bot:
             continue
 
-        if "!randompuzzle" in message.content:
+        print("Repeating:", message.content)
 
-            await channel.send("checked2")
+        await channel.send(message.content)
 
-            save_last_command(message.id)
-            break
+    print("Finished test")
 
     await client.close()
 
